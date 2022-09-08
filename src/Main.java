8faaa237fa84c40fee2f090d59388232fa7ec75a
Ирина <mi.irene97@gmail.com>
2022-09-08 17:43:14 +0300

    Задачи 1, 2 и 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
@@ -12,7 +12,7 @@ public class Main {
         System.out.println(cat);
         paper = paper + 4;
         System.out.println(paper);
-        dog = dog - 3;
+        dog = dog - 3.5;
         System.out.println(dog);
         cat = cat - 1.6;
         System.out.println(cat);
